eb286033fb080f63850b2245305f6966e50a4cea
damarislh10 <cortezhernandezdamaris@gmail.com>
2022-03-26 16:54:20 -0500

    Win players

--- tic-tac-toe/App.js
+++ tic-tac-toe/App.js
@@ -1,44 +1,93 @@
-import React,{useState} from "react"
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Box from './components/Box'
+import React, { useState, useEffect } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import Box from "./components/Box";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function App() {
+  const [boxes, setBoxes] = useState(Array(9).fill(null));
+  const [isXChance, setIsXChance] = useState(true);
+  const [winner, setWinner] = useState(null);
 
-  const [boxes,setBoxes] = useState(Array(9).fill(null))
-  const [isXChance,setIsXChance] = useState(true)
-  const [winner, setWinner] = useState(null)
-
-  function PlayBox(no){
+  function PlayBox(no) {
     return (
       <Box
-        no ={no}
-        boxInfo={{boxes,setBoxes}}
-        chance = {{isXChance, setIsXChance}}
+        no={no}
+        boxInfo={{ boxes, setBoxes }}
+        chance={{ isXChance, setIsXChance }}
         winner={winner}
       />
-    )
+    );
+  }
+
+  const winPosition = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  function calculateWin() {
+    for (let i = 0; i < winPosition.length; i++) {
+      if (
+        boxes[winPosition[i][0]] !== null &&
+        boxes[winPosition[i][0]] === boxes[winPosition[i][1]] &&
+        boxes[winPosition[i][0]] === boxes[winPosition[i][2]]
+      ) {
+        setWinner(boxes[winPosition[i][0]]);
+        return;
+      }
+    }
+  }
+
+  useEffect(() => {
+    calculateWin();
+  }, [isXChance]);
+  function resetValues() {
+    setWinner(null);
+    setBoxes(Array(9).fill(null));
+    setIsXChance(true);
   }
   return (
     <View style={styles.container}>
       <StatusBar style="auto" backgroundColor="green" />
+      <View style={styles.featureContainer}>
+        {winner !== null ? (
+          <Text style={[styles.primaryText, styles.winnerText]}>
+            {winner} Gano!
+          </Text>
+        ) : (
+          <Text style={styles.primaryText}>
+            El turno de: {isXChance ? "X" : "O"}
+          </Text>
+        )}
+        <Ionicons
+          style={styles.resetIcon}
+          name="reload-circle"
+          size={38}
+          color="black"
+          onPress={resetValues}
+        />
+      </View>
       <View style={styles.playBoard}>
         <View style={styles.rows}>
-        {PlayBox(0)}
-        {PlayBox(1)}
-        {PlayBox(2)}
+          {PlayBox(0)}
+          {PlayBox(1)}
+          {PlayBox(2)}
         </View>
         <View style={styles.rows}>
-        {PlayBox(3)}
-        {PlayBox(4)}
-        {PlayBox(5)}
+          {PlayBox(3)}
+          {PlayBox(4)}
+          {PlayBox(5)}
         </View>
         <View style={styles.rows}>
-        {PlayBox(6)}
-        {PlayBox(7)}
-        {PlayBox(8)}
+          {PlayBox(6)}
+          {PlayBox(7)}
+          {PlayBox(8)}
         </View>
-
       </View>
     </View>
   );
@@ -47,16 +96,36 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
-  playBoard:{
-    borderWidth:10,
-    borderRadius:10,
-    borderColor:'green'
+  playBoard: {
+    borderWidth: 10,
+    borderRadius: 10,
+    borderColor: "green",
   },
   rows: {
-    flexDirection:'row'
-  }
+    flexDirection: "row",
+  },
+  resetIcon: {
+    position: "absolute",
+    right: 20,
+  },
+  featureContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    width: "100%",
+    marginBottom: 20,
+  },
+  primaryText: {
+    fontSize: 36,
+    color: "black",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  winnerText: {
+    color: "darkgreen",
+    fontSize: 48,
+  },
 });

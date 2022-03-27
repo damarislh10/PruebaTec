374f15f2c83f42bcbd04079e0f1a4d5dc355119d
damarislh10 <cortezhernandezdamaris@gmail.com>
2022-03-26 21:00:06 -0500

    Style borde in playBoard

--- tic-tac-toe/App.js
+++ tic-tac-toe/App.js
@@ -101,7 +101,7 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   playBoard: {
-    borderWidth: 10,
+    borderWidth: 6,
     borderRadius: 10,
     borderColor: "green",
   },
